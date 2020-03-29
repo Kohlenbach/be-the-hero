2323d6b9cf7006204f3bb9029c0d7caeff05363f
Lukas <kohle.lukas@gmail.com>
2020-03-29 16:40:57 -0300

    Adding redirect in register page

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import api from '../../services/api'
 import './styles.css'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg'
 
@@ -11,6 +11,7 @@ export default function Register() {
     const [whatsapp, setWhatsapp] = useState('')
     const [city, setCity] = useState('')
     const [uf, setUf] = useState('')
+    const history = useHistory()
 
     async function handleRegister(e) {
         e.preventDefault()
@@ -27,6 +28,8 @@ export default function Register() {
             const response = await api.post('ongs', data)
             
             alert(`Seu ID de acesso: ${response.data.id}`)
+
+            history.push('/')
         } catch (error) {
             alert('Erro no cadastro, tente novamente.')
         }
